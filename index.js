54bc05303029c44f709bf11270c2778df3135fc2
Ara Yeressian <ayeressian2@gmail.com>
2016-10-07 19:18:45 +0400

    .

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const request = require('request'),
 
 class ImageNameProvider {
     execute(input) {
-        const formData = {
-            image: fs.createReadStream(input.imagePath)
-        };
+        return Promise((resolve, reject) => {
+            const formData = {
+                image: fs.createReadStream(input.imagePath)
+            };
 
-        request.post({ url: 'https://www.wolframcloud.com/objects/eca9494d-1dfd-48c4-b063-e2a98f66d526', formData }, (err, httpResponse, body) => {
-            if (err) {
-                return console.error('upload failed:', err);
-            }
-            body = body.substring(0, body.length - 1);
-            body = body.substring(1);
-            console.log(body);
+            request.post({ url: 'https://www.wolframcloud.com/objects/eca9494d-1dfd-48c4-b063-e2a98f66d526', formData }, (err, httpResponse, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    body = body.substring(0, body.length - 1);
+                    body = body.substring(1);
+                    resolve(body);
+                }
+            });
         });
     }
 }
