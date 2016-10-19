f8a451e8025b4861bd2749a7c21183b4179c9ac9
Ara Yeressian <ayeressian2@gmail.com>
2016-10-19 17:55:22 +0400

    .

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ class ImageNameProvider {
             // request.post({ url: 'https://www.wolframcloud.com/objects/eca9494d-1dfd-48c4-b063-e2a98f66d526', formData }, (err, httpResponse, body) => {
             request.post({ url: 'https://www.wolframcloud.com/objects/c99593f9-3ef4-461a-ba8c-f40ad1d2a602', formData }, (err, httpResponse, body) => {
                 if (err) {
-                    reject(err);
+                    resolve('A thing');
                 } else {
                     if (body.length < 3) {
-                        reject('no_result');
+                        resolve('A thing');
                     } else {
                         body = body.substring(0, body.length - 1);
                         body = body.substring(1);
